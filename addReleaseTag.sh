747665756313acc48825b5b477c4efe1e677bc2b
Rishabh Poddar <rishabh.poddar@gmail.com>
2020-04-03 17:38:46 +0530

    changes to release script

--- addReleaseTag.sh
+++ addReleaseTag.sh
@@ -71,7 +71,7 @@ then
     else
         git merge -s ours master
         git checkout master
-        git merge $branch_name
+        git merge origin/$branch_name
         git push
         git checkout $branch_name
         echo "Done!"
